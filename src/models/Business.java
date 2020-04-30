e2076b8f6e917377a1095fa2c0d042661695c9dd
taihei.kakefuda <tkhs0302aigis8@outlook.jp>
2020-04-30 14:43:47 +0900

    Add Business class

---
+++ src/models/Business.java
@@ -0,0 +1,112 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Table(name = "businesses")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllbusinesses",
+        query = "SELECT b FROM Business AS b ORDER BY b.id DESC"
+        ),
+    @NamedQuery(
+        name = "getBusinessesCount",
+        query = "SELECT COUNT(b) FROM Business AS b"
+        ),
+})
+@Entity
+public class Business {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBusiness_opportunities() {
+        return business_opportunities;
+    }
+
+    public void setBusiness_opportunities(String business_opportunities) {
+        this.business_opportunities = business_opportunities;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdate_at() {
+        return update_at;
+    }
+
+    public void setUpdate_at(Timestamp update_at) {
+        this.update_at = update_at;
+    }
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "business_opportunities", length = 255, nullable = false)
+    private String business_opportunities;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "update_at", nullable = false)
+    private Timestamp update_at;
+
+}
