d485485a8306625e9d9a44650da44a0b744cb1d8
taihei.kakefuda <tkhs0302aigis8@outlook.jp>
2020-05-07 14:44:38 +0900

    Add Business edit

---
+++ src/controllers/business/BusinessesEditServlet.java
@@ -0,0 +1,53 @@
+package controllers.business;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Business;
+import models.Employee;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class BusinessesEditServlet
+ */
+@WebServlet("/businesses/edit")
+public class BusinessesEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BusinessesEditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Business b = em.find(Business.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(b != null && login_employee.getId() == b.getEmployee().getId()) {
+            request.setAttribute("business", b);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("report_id", b.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
